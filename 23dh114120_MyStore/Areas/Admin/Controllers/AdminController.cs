f510d5b4a6a6a570dfd37887d2ae135b930806ed
SV <SV@PM19-PC09>
2024-10-08 11:30:41 +0700

    Add project files.

---
+++ 23dh114120_MyStore/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23dh114120_MyStore.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin/Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 23dh114120_MyStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_23dh114120_MyStore.MvcApplication" Language="C#" %>
